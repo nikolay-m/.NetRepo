2294156347e6fe43c023d3fce6fc22a4255f4ee6
nikolay-m <nikolay.malenko@gmail.com>
2013-09-03 20:49:20 +0300

    2

---
+++ Malenko_new/Malenko_new/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Malenko_new.Models;
+
+namespace Malenko_new.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            DataContext _db = new DataContext();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Users.Add(user);
+                    _db.SaveChanges();
+
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex);
+                }
+            }
+            return RedirectToAction("Create");
+        }
+
+        [HttpGet]
+        public ActionResult GetUsers()
+        {
+            DataContext _db = new DataContext();
+            var res = _db.Users;
+            return View(res);
+        }
+    }
+}
---
+++ Malenko_new/Malenko_new/Scripts/Container.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+
+    var found = false;
+
+    $("#tab-container ul li").click(function () {
+        $("#tab-container ul li").each(function () {
+            $(this).removeClass("selected");
+        });
+
+        var active = "#".concat($(this).attr("id"));
+        var activeContainer = "#c".concat($(this).attr("id"));
+        
+        (function ($) {
+            jQuery.fn.hideDiv = function () {
+                $("#main-container div").each(function () {
+                    $("#main-container div h3").each(function () {
+                        $(this).next("div").fadeOut();
+                    });
+                    $(this).hide();
+                });
+            };
+        })(jQuery);
+
+        (function ($) {
+            jQuery.fn.showDiv = function (container) {
+                $(container).show("slide", { direction: "down" }, 100, function () {
+                    $(container.concat(" div")).show("slide", { direction: "left" }, 1000);
+                });
+                
+            }
+        })(jQuery);
+
+        $(active).addClass("selected");
+        $().hideDiv();
+        $().showDiv(activeContainer);
+    });
+        if (!found) {
+            $("#tab-container li:first").addClass("selected");
+            $("#main-container div:not(first)").hide();
+            $("#main-container div:first").show("slide", { direction: "down" }, 200,function () {
+                $(this).children(".text").show("slide", {direction:"left"}, 1000);
+            });
+        }
+});
+
