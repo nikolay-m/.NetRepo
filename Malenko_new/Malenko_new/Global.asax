5e785be53e636f4d2cc814a078a1445fed9db18b
nikolay-m <nikolay.malenko@gmail.com>
2013-09-03 20:29:44 +0300

    MalenkoNew

---
+++ Malenko_new/Malenko_new/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Malenko_new.MvcApplication" Language="C#" %>
---
+++ Malenko_new/Malenko_new/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Malenko_new.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("Userdb")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
